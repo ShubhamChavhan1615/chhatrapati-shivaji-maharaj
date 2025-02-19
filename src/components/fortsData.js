23cb54bc86ad91656db84f1324e49cfdbb6f8e9b
Shubham Chavhan <shubhamchavhan908@gmail.com>
2025-02-19 09:53:12 +0530

    ...

--- src/components/fortsData.js
+++ src/components/fortsData.js
@@ -1,3 +1,19 @@
+import raygad from "../../public/raygad.jfif";
+import pratapgad from '../../public/Pratapgad.jpg'
+import s from '../../public/s.jfif'
+import rajgad from '../../public/rajgad.jpg'
+import torna from '../../public/torna.jfif'
+import lohgad from '../../public/Lohagad.jpg'
+import visapur from '../../public/visapur.jfif'
+import shivneri from '../../public/shivneri.jfif'
+import panhala from '../../public/panhala.jpg'
+import salher from '../../public/salher.jpg'
+import sajjangad from '../../public/sajjangad.jfif'
+import anjanvel from '../../public/anjanvel.jfif'
+import sindudurg from '../../public/sindudurg.jfif'
+import pavangad from '../../public/pavngad.jpg'
+import ahivantgad from '../../public/avihantgad.jpg'
+
 export const kille = [
   {
     name: "रायगड",
@@ -5,7 +21,7 @@ export const kille = [
     description:
       "स्वराज्याचे तेजस्वी प्रतीक. छत्रपती शिवाजी महाराजांचा राज्याभिषेक येथे ६ जून १६७४ रोजी मोठ्या थाटामाटात पार पडला. हा किल्ला मराठा साम्राज्याच्या वैभवाचे प्रतिक असून, इथूनच त्यांनी हिंदवी स्वराज्याचा विस्तार केला. येथे आजही महाराजांच्या समाधी सोबतच भवानी टोक, टकमक टोक आणि होळीची महाल ही प्रमुख स्थळे पाहण्यास मिळतात.",
     image:
-      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcRZT3mFcOiNy1cezbPX6-pKnrCaX2rA1ruv6x4q0LpKiEdD-IK2BL3XeMyydKGQbio-23bczwH5Kn9ksBKg5nRSkJQWt4oxS4xbqzqCXRE",
+      raygad,
     x: "30%",
     y: "50%",
     location: [18.234, 73.432],
@@ -16,7 +32,7 @@ export const kille = [
     description:
       "मराठा साम्राज्याच्या विजयाचा साक्षीदार. १० नोव्हेंबर १६५९ रोजी येथे शिवाजी महाराज आणि अफजलखान यांच्यातील ऐतिहासिक लढाई झाली, ज्यात महाराजांनी चातुर्याने अफजलखानाचा पराभव केला. किल्ल्यावर भवानी मातेचे मंदिर आणि अफजलखानाची कबर देखील आहे.",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Pratapgad_The_Fort_of_Valour.jpg/450px-Pratapgad_The_Fort_of_Valour.jpg",
+      pratapgad,
     x: "40%",
     y: "60%",
     location: [18.234, 73.432],
@@ -27,7 +43,7 @@ export const kille = [
     description:
       "(पूर्वीचा कोंढाणा किल्ला) तानाजी मालुसरेंच्या अद्वितीय शौर्याची साक्ष असलेला किल्ला. १६७० मध्ये मराठ्यांनी हा किल्ला परत जिंकला. 'गड आला पण सिंह गेला' ही इतिहास प्रसिद्ध कहाणी इथेच घडली. आजही गडावर तानाजी मालुसरेंचे स्मारक आणि ऐतिहासिक दरवाजे पाहायला मिळतात.",
     image:
-      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcSP77uAFFgCDiwUrBiRso5jHFtT_Jrjsw5UmKM9DUt6f6i9X4sxeDicf5mIEubAUuhyQAocSh1y1z8YD9ephdwZfkEcLrrb9SZ734oaZHQ",
+      s,
     x: "50%",
     y: "40%",
     location: [18.234, 73.432],
@@ -38,7 +54,7 @@ export const kille = [
     description:
       "छत्रपती शिवाजी महाराजांचे पहिले राजधानीचे ठिकाण. जवळपास २५ वर्षे महाराजांनी इथून राज्यकारभार चालवला. किल्ल्यावर बालेकिल्ला, सुवेळा माची, संजीवनी माची यांसारखी मजबूत बांधणी आजही आकर्षण ठरते.",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Bali_Khila_Rajgad_Maharashtra.jpg/600px-Bali_Khila_Rajgad_Maharashtra.jpg",
+      rajgad,
     x: "60%",
     y: "30%",
     location: [18.234, 73.432],
@@ -49,7 +65,7 @@ export const kille = [
     description:
       "छत्रपती शिवाजी महाराजांनी अवघ्या १६ व्या वर्षी जिंकलेला पहिला किल्ला. यालाच 'प्रचंडगड' असेही म्हणतात. स्वराज्य स्थापनेची खरी सुरुवात इथून झाली. गडाच्या माथ्यावर झुंजार माची आणि बुद्धलिंग मंदिर विशेष प्रसिद्ध आहे.",
     image:
-      "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQlhIwHs4NA8z_z0N3KnQFv9nl0TmWGtisH9qQX-r4k_CMxPpIgnL6CCBx7hcRuZH9jQiftIHaVc9MKyZ1kPSWt3J95TXZX_pwWhMdIlw",
+      torna,
     x: "70%",
     y: "50%",
     location: [18.234, 73.432],
@@ -60,7 +76,7 @@ export const kille = [
     description:
       "मराठा साम्राज्याच्या सामर्थ्याचे प्रतीक. १६४८ मध्ये शिवाजी महाराजांनी हा किल्ला जिंकला आणि नंतर पेशव्यांच्या काळातही याचा उपयोग झाला. पावसाळ्यात येथील ‘विनचूकाटा’ आणि गडावरील भव्य दरवाजे पर्यटकांना भुरळ घालतात.",
     image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Lohagad_fort.jpg/360px-Lohagad_fort.jpg",
+      lohgad,
     x: "80%",
     y: "40%",
     location: [18.234, 73.432],
@@ -71,7 +87,7 @@ export const kille = [
     description:
       "लोहगडच्या शेजारी असलेला हा किल्ला १६५० मध्ये शिवाजी महाराजांनी जिंकला. याची भव्य तटबंदी आणि ऐतिहासिक गुहा आजही भव्यतेची साक्ष देतात.",
     image:
-      "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcSd2LLioCdQYEfDSUS0Wj9ybtMxR3z3W4n52TQ4EXvZxpg6ik-jVi8nSyN_wv2PNFh_iWWYTIFfwQP_s0p-yAYf0q-3MMMX6YPEopaye0Q",
+      visapur,
     x: "90%",
     y: "60%",
     location: [18.234, 73.432],
@@ -82,7 +98,7 @@ export const kille = [
     description:
       "छत्रपती शिवाजी महाराजांचा जन्म याच किल्ल्यावर १९ फेब्रुवारी १६३० रोजी झाला. स्वराज्याचे बीज येथेच रुजले. किल्ल्यावर शिवजन्मस्थळ, गंगा-जमुना टाकी, सात दरवाजे आणि कडे टोक ही प्रमुख स्थळे आहेत.",
     image:
-      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQcftuZJKYTBLLGke0yGfQ794Kpifk1QAvHtPGYrkMvhfZPITj0Efl-Ze50gZwNam2zjw8cXF5DXsEUmC7dVCkZmshpQaoLghS1WznnlA",
+      shivneri,
     x: "85%",
     y: "75%",
     location: [18.234, 73.432],
@@ -93,7 +109,7 @@ export const kille = [
     description:
       "हा किल्ला महाराजांच्या रणनीतीचा साक्षीदार. १६६० मध्ये सिद्धी जोहरच्या वेढ्यातून महाराजांनी चतुराईने सुटका केली. किल्ल्यावर अंधारबाव, अंबरखाना आणि साजिरा-काजिरा बुरूज प्रसिद्ध आहेत.",
     image:
-      "https://lh5.googleusercontent.com/p/AF1QipPjF7PYzVfSV4-eRk7dGSfnGf9Q0ZZ5WZTYEBju=w810-h468-n-k-no",
+      panhala,
     x: "70%",
     y: "50%",
     location: [18.234, 73.432],
@@ -104,7 +120,7 @@ export const kille = [
     description:
       "महाराष्ट्रातील सर्वात उंच किल्ला. इथे १६७२ मध्ये मराठ्यांनी मुघलांवर मोठा विजय मिळवला. या लढाईत मराठ्यांनी उत्तरेतही विजय मिळवण्याची क्षमता दाखवली. गडावर परशुराम मंदिर आणि गुहा पाहण्यासारख्या आहेत.",
     image:
-      "https://lh5.googleusercontent.com/p/AF1QipPsVGF6jrjh4fPDz5aalY47rGpewa9Q-oaR4DQK=w810-h468-n-k-no",
+      salher,
     x: "60%",
     y: "30%",
     location: [18.234, 73.432],
@@ -115,7 +131,7 @@ export const kille = [
     description:
       "धार्मिक आणि ऐतिहासिकदृष्ट्या महत्त्वाचा किल्ला. हा किल्ला शिवाजी महाराजांनी समर्थ रामदास स्वामींना दिला होता. येथे आजही समर्थांची समाधी आहे, ज्याला अनेक भक्त भेट देतात.",
     image:
-      "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcRfqR4q2PyU4QWuBOIKi9O0Z-aHnvrMSZzVfaliE8hmi1kv6PMk_vHS9jYt31H21T2_RrLndJ_3Y1IKP9a-8Qj8THdBm4JLUUVVDbucr4I",
+      sajjangad,
     x: "40%",
     y: "60%",
     location: [17.648, 73.833],
@@ -126,7 +142,7 @@ export const kille = [
     description:
       "कोंकण किनारपट्टीवरील एक महत्त्वाचा जलदुर्ग. शिवाजी महाराजांनी याचा उपयोग स्वराज्याच्या सागरी संरक्षणासाठी केला होता.",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpL0j-Jcel3XnuALYwu_ri02aZDN2Wg54nDA&s",
+      anjanvel,
     x: "55%",
     y: "70%",
     location: [17.550, 73.121],
@@ -137,7 +153,7 @@ export const kille = [
     description:
       "शिवाजी महाराजांनी सागरी संरक्षणासाठी हा भव्य जलदुर्ग बांधला. या किल्ल्यात शिवरायांचे मंदीर असून, समुद्राच्या मधोमध उभा असलेला हा किल्ला मराठा आरमाराचे प्रतीक आहे.",
     image:
-      "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcS2Pd9gwbUbNqN4NWUVoa0x74Rcd9IzebuEfXQEy0D6afU6w_zkSdVO2y4Y3i31ibyQsHHWHXCvC373bNSf1HNGCz7TRdJJt0suNAwu-w",
+      sindudurg,
     x: "65%",
     y: "80%",
     location: [16.038, 73.463],
@@ -148,7 +164,7 @@ export const kille = [
     description:
       "गडहिंग्लज जवळील हा किल्ला पन्हाळ्याच्या संरक्षणासाठी महत्त्वाचा होता. छत्रपती शिवाजी महाराजांनी येथे मोठ्या विजय मिळवला होता.",
     image:
-      "https://spothunter.in/wp-content/uploads/2022/02/%E0%A4%AA%E0%A4%BE%E0%A4%B5%E0%A4%A8%E0%A4%97%E0%A4%A1-%E0%A4%95%E0%A4%BF%E0%A4%B2%E0%A5%87-%E0%A4%95%E0%A4%BE-%E0%A4%87%E0%A4%A4%E0%A4%BF%E0%A4%B9%E0%A4%BE%E0%A4%B8.jpg",
+      pavangad,
     x: "50%",
     y: "85%",
     location: [16.826, 74.393],
@@ -159,7 +175,7 @@ export const kille = [
     description:
       "साल्हेरच्या लढाईच्या वेळी शिवाजी महाराजांनी हा किल्ला जिंकला. डोंगराळ प्रदेशात वसलेला हा किल्ला रणनीतीच्या दृष्टिकोनातून महत्त्वाचा होता.",
     image:
-      "https://lh5.googleusercontent.com/p/AF1QipOi1yLTOSwjIaAnRFANGp8RurxpGWVjtnRgam66=w810-h468-n-k-no",
+      ahivantgad,
     x: "35%",
     y: "40%",
     location: [20.347, 73.918],
